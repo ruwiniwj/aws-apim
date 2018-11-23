639a37c54ffe9b3b60ca87cc88bfda1f4e836465
madeesha <madeeshafernando@gmail.com>
2018-11-23 20:26:55 +0530

    add apim 2.6.0 puppet module and CF scripts

--- apim/Minimum-HA/puppet/apim/manifests/custom.pp
+++ apim/Minimum-HA/puppet/apim/manifests/custom.pp
@@ -1,4 +1,4 @@
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 #  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,18 +12,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
-# For custom configurations
-class apim::custom inherits apim::params {
-
-  if $enable_test_mode == 'true' {
-    file { "/usr/lib/wso2/wso2am/2.1.0/wso2am-2.1.0/repository/components/lib/jacoco-agent.jar":
-      owner  => $user,
-      group  => $user_group,
-      mode   => '0754',
-      source => "puppet:///modules/installers/jacoco-agent.jar",
-    }
-  }
-
+# Class: apim::custom
+# This class is reserved to run custom user code before starting the server.
+class apim::custom {
 }
